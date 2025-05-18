533b2959782736a01e595a43770bbe7e20e98726
minmus-0-o <glebsapsan@gmail.com>
2025-05-18 19:15:53 +0300

    1.1 Появилась возможность добавлять рейсы

--- Bus_station/tickets/urls.py
+++ Bus_station/tickets/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.trip_list, name='trip_list'),
     path('order/<int:trip_id>/', views.create_order, name='create_order'),
+    path('add_trip/', views.add_trip, name='add_trip'),
 ]
--- Bus_station/tickets/views.py
+++ Bus_station/tickets/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Trip, Order
 from django.utils import timezone
+from django.contrib.admin.views.decorators import staff_member_required
+from django import forms
+import datetime
+
+class TripForm(forms.ModelForm):
+    class Meta:
+        model = Trip
+        fields = ['origin', 'destination', 'departure_time', 'price', 'seats_available']
+        widgets = {
+            'departure_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 def trip_list(request):
     trips = Trip.objects.filter(departure_time__gte=timezone.now(), seats_available__gt=0)
@@ -23,3 +34,14 @@ def create_order(request, trip_id):
             trip.save()
             return redirect('trip_list')
     return render(request, 'tickets/create_order.html', {'trip': trip})
+
+@staff_member_required
+def add_trip(request):
+    if request.method == 'POST':
+        form = TripForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('trip_list')
+    else:
+        form = TripForm()
+    return render(request, 'tickets/add_trip.html', {'form': form})
